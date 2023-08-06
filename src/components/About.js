135a8370d8f9f681386e83bc526a62e944f16228
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-08-05 18:57:01 -0700

    props basics lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,20 @@
 import React from "react";
+import Links from "./Links";
+import user from "../data/user";
 
-function About() {
+function About(props) {
+const isBio = props.bio
+console.log(isBio)
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{isBio ? isBio : null}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={user.links.github} linkedin={user.links.linkedin}
+      />
     </div>
-  );
+  )
 }
 
-export default About;
+export default About
